746bfd7e76283a34150fff8116dc397e13021236
Sean Chen <sean.chen@leocorn.com>
2019-09-19 18:46:17 +0000

    create the script to install vim plugins.

---
+++ scripts/quick-setup.sh
@@ -0,0 +1,15 @@
+# /bin/bash
+
+# install view-pathogen.
+mkdir -p ~/.vim/autoload
+mkdir -p ~/.vim/bundle
+
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Install the AWESOME git wrapper in vim!
+cd ~/.vim/bundle
+git clone https://github.com/tpope/vim-fugitive.git
+
+# install vim-vue syntax highlighting
+cd ~/.vim/bundle
+git clone https://github.com/posva/vim-vue.git
